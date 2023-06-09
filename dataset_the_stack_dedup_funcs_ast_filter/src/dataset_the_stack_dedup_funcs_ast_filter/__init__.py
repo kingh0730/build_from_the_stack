3b0e00e41fb3be2d2ac271d854e441e5566080ce
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-08 21:00:05 -0700

    works

--- dataset_the_stack_dedup_funcs_ast_filter/src/dataset_the_stack_dedup_funcs_ast_filter/__init__.py
+++ dataset_the_stack_dedup_funcs_ast_filter/src/dataset_the_stack_dedup_funcs_ast_filter/__init__.py
@@ -1,3 +1,7 @@
+# Stdlib imports
+import logging
+
+# Project imports
 from .dataset import TheStackDedupFuncsAstFilter
 
 
@@ -5,7 +9,15 @@ def main():
     """Entry point for the application script"""
     print("Call your main application code here")
 
+    logging.basicConfig(
+        level=logging.INFO,
+    )
+
+    logger = logging.getLogger(__name__)
+
     global ds
 
-    the_stack_dedup = TheStackDedupFuncsAstFilter()
+    the_stack_dedup = TheStackDedupFuncsAstFilter(
+        logger=logger,
+    )
     ds = the_stack_dedup.dataset()
