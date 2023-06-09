84776e2149ff14a71e377bef0e439e5ca3f616f6
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 13:17:01 -0700

    maybe

--- scripts/check_version_not_done.sh
+++ scripts/check_version_not_done.sh
@@ -7,16 +7,21 @@ directories=$(find . -maxdepth 1 -type d)
 for directory in $directories; do
     # Skip the current directory and parent directory
     if [[ $directory != "." && $directory != ".." ]]; then
+        # Remove the starting './' from the directory name
+        directory=$(basename "$directory")
         # Check if pyproject.toml exists
         if [ -f "$directory/pyproject.toml" ]; then
+
             # Extract the version from pyproject.toml
             version=$(awk -F'"' '/^version = / {print $2}' "$directory/pyproject.toml")
 
+            done_branch="done/$directory$version"
+
             # Check if the git branch exists
-            if git show-ref --quiet "refs/heads/$directory$version"; then
-                echo "Git branch $directory$version exists."
+            if git show-ref --quiet "refs/heads/$done_branch"; then
+                echo "Git branch $done_branch exists."
             else
-                echo "Git branch $directory$version does not exist."
+                echo "Git branch $done_branch does not exist."
             fi
         fi
     fi
