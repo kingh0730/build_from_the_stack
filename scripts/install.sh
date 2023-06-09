2336b7a95d8d13f515e1db379dc0ce0b19ca2a39
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-08 23:28:37 -0700

    alright

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
 pip install \
     -e "./dataset_the_stack_dedup [dev,test]" \
-    -e "./dataset_the_stack_dedup_funcs_ast_filter [dev,test]"
+    -e "./dataset_the_stack_dedup_funcs_ast_filter [dev,test]" \
+    -e "./dataset_the_stack_dedup_append_imports_stats [dev,test]"
