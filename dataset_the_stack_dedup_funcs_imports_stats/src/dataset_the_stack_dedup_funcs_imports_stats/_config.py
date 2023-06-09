94b77b91d964cf784b5cc3096f1cf7d10a6f1547
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 01:17:55 -0700

    yeah?

--- dataset_the_stack_dedup_funcs_imports_stats/src/dataset_the_stack_dedup_funcs_imports_stats/_config.py
+++ dataset_the_stack_dedup_funcs_imports_stats/src/dataset_the_stack_dedup_funcs_imports_stats/_config.py
@@ -1,7 +1,17 @@
 # Stdlib imports
+from os import path, cpu_count
 from importlib.metadata import version
 
 
 BASE_CACHE_DIR = "E:/build_from_the_stack/cache/"
 _dir = __package__ + version(__package__)
 CACHE_DIR = BASE_CACHE_DIR + _dir
+
+
+TOP_PYPI_PACKAGES_JSON = (
+    path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+    + "/data/top_pypi_packages/top-pypi-packages-30-days.json"
+)
+
+
+NUM_PROC = cpu_count()
