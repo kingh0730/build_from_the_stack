d15a733c33478dc3ef7dc30adca14e885d6851e5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-08 19:09:07 -0700

    maybe?

--- src/sample/build_files/the_stack_dedup.py
+++ src/sample/build_files/the_stack_dedup.py
@@ -5,11 +5,18 @@ from datasets import load_dataset
 from ._config import THE_STACK_DEDUP_CACHE_DIR
 
 
-def the_stack():
-    ds = load_dataset(
-        "bigcode/the-stack-dedup",
-        data_dir="data/python",
-        cache_dir=THE_STACK_DEDUP_CACHE_DIR,
-    )
+class TheStackDedupDataset:
+    def __init__(self):
+        self._ds = None
 
-    return ds
+    def loads(self, streaming=True):
+        self._ds = load_dataset(
+            "bigcode/the-stack-dedup",
+            data_dir="data/python",
+            streaming=streaming,
+            cache_dir=THE_STACK_DEDUP_CACHE_DIR,
+        )
+        return self._ds
+
+    def dataset(self):
+        return self._ds
