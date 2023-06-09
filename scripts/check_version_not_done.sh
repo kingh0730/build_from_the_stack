931bc5ce2cdf8fa100de683981fdb34f047b6bb5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 13:11:54 -0700

    ok

--- scripts/check_version_not_done.sh
+++ scripts/check_version_not_done.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# Get the current directory
-current_dir=$(pwd)
+# Get the list of child directories
+directories=$(find . -maxdepth 1 -type d)
 
-# Loop through each child directory
-for dir in "$current_dir"/*/; do
-    # Check if the child directory has a pyproject.toml file
-    if [ -f "$dir/pyproject.toml" ]; then
-        # Change into the child directory
-        cd "$dir"
+# Iterate over each child directory
+for directory in $directories; do
+    # Skip the current directory and parent directory
+    if [[ $directory != "." && $directory != ".." ]]; then
+        # Check if pyproject.toml exists
+        if [ -f "$directory/pyproject.toml" ]; then
+            # Extract the version from pyproject.toml
+            version=$(awk -F= '/^version/ { gsub(/[[:blank:]]/, "", $2); print $2 }' "$directory/pyproject.toml")
 
-        # Extract the version from pyproject.toml
-        version=$(awk -F= '/^version/ { gsub(/[[:blank:]]/, "", $2); print $2 }' "$directory/pyproject.toml")
-
-        # Check if the git branch exists
-        if git show-ref --quiet "refs/heads/$directory$version"; then
-            echo "Git branch $directory$version exists."
-        else
-            echo "Git branch $directory$version does not exist."
+            # Check if the git branch exists
+            if git show-ref --quiet "refs/heads/$directory$version"; then
+                echo "Git branch $directory$version exists."
+            else
+                echo "Git branch $directory$version does not exist."
+            fi
         fi
-
-        # Change back to the original directory
-        cd "$current_dir"
     fi
 done
