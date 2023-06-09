fa8fd8784ac9502df13837c2757ec7bc4cef1f2b
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 12:26:43 -0700

    copy

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the current directory
+current_dir=$(pwd)
+
+# Loop through each child directory
+for dir in "$current_dir"/*/; do
+    # Check if the child directory has a pyproject.toml file
+    if [ -f "$dir/pyproject.toml" ]; then
+        # Change into the child directory
+        cd "$dir"
+
+        # Run 'tox' command
+        python -m tox -e py
+
+        # Change back to the original directory
+        cd "$current_dir"
+    fi
+done
