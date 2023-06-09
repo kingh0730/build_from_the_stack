2ca4c446a87136bc0d246424ab40724906ed507a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 12:47:36 -0700

    no need

--- scripts/test_tox.sh
+++ scripts/test_tox.sh
@@ -10,21 +10,6 @@ for dir in "$current_dir"/*/; do
         # Change into the child directory
         cd "$dir"
 
-        # Run 'build' command
-        pip install -e .
-
-        # Change back to the original directory
-        cd "$current_dir"
-    fi
-done
-
-# Loop through each child directory
-for dir in "$current_dir"/*/; do
-    # Check if the child directory has a pyproject.toml file
-    if [ -f "$dir/pyproject.toml" ]; then
-        # Change into the child directory
-        cd "$dir"
-
         # Run 'tox' command
         python -m tox -e py
 
