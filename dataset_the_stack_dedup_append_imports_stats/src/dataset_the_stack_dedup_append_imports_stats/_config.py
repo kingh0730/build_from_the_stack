ee4fa280633d6414ed125d0c56faf8023b106801
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 00:01:44 -0700

    fine

--- dataset_the_stack_dedup_append_imports_stats/src/dataset_the_stack_dedup_append_imports_stats/_config.py
+++ dataset_the_stack_dedup_append_imports_stats/src/dataset_the_stack_dedup_append_imports_stats/_config.py
@@ -3,7 +3,8 @@ from importlib.metadata import version
 
 
 BASE_CACHE_DIR = "E:/build_from_the_stack/cache/"
-
 _dir = __package__ + version(__package__)
-
 CACHE_DIR = BASE_CACHE_DIR + _dir
+
+
+TOP_PYPI_PACKAGES_JSON = "../data/top_pypi_packages/top-pypi-packages-30-days.json"
--- dataset_the_stack_dedup_funcs_ast_filter/src/dataset_the_stack_dedup_funcs_ast_filter/_config.py
+++ dataset_the_stack_dedup_funcs_ast_filter/src/dataset_the_stack_dedup_funcs_ast_filter/_config.py
@@ -3,9 +3,7 @@ from importlib.metadata import version
 
 
 BASE_CACHE_DIR = "E:/build_from_the_stack/cache/"
-
 _dir = __package__ + version(__package__)
-
 CACHE_DIR = BASE_CACHE_DIR + _dir
 
 
