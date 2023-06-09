6f022131bec126c6967222fa4c05c9402382ba8b
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 12:56:00 -0700

    change

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,19 @@
-pip install \
-    -e "./dataset_the_stack_dedup [dev,test]" \
-    -e "./dataset_the_stack_dedup_funcs_ast_filter [dev,test]" \
-    -e "./dataset_the_stack_dedup_append_imports_stats [dev,test]" \
-    -e "./dataset_the_stack_dedup_funcs_imports_stats [dev,test]"
+#!/bin/bash
+
+# Get the current directory
+current_dir=$(pwd)
+
+# Loop through each child directory
+for dir in "$current_dir"/*/; do
+    # Check if the child directory has a pyproject.toml file
+    if [ -f "$dir/pyproject.toml" ]; then
+        # Change into the child directory
+        cd "$dir"
+
+        # Run 'install' command
+        python -m pip install -e .
+
+        # Change back to the original directory
+        cd "$current_dir"
+    fi
+done
