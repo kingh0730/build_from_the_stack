3eef0994bcf33a1b959a42991ef8d2eeb302a5eb
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 22:30:23 -0700

    ok

--- scripts/check_versions_done.sh
+++ scripts/check_versions_done.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
-# Get the list of child directories
-directories=$(find . -maxdepth 1 -type d)
+# # Get the list of child directories
+# directories=$(find . -maxdepth 1 -type d)
 
-# Iterate over each child directory
-for directory in $directories; do
-    # Skip the current directory and parent directory
-    if [[ $directory != "." && $directory != ".." ]]; then
-        # Remove the starting './' from the directory name
-        directory=$(basename "$directory")
-        # Check if pyproject.toml exists
-        if [ -f "$directory/pyproject.toml" ]; then
+# # Iterate over each child directory
+# for directory in $directories; do
+#     # Skip the current directory and parent directory
+#     if [[ $directory != "." && $directory != ".." ]]; then
+#         # Remove the starting './' from the directory name
+#         directory=$(basename "$directory")
+#         # Check if pyproject.toml exists
+#         if [ -f "$directory/pyproject.toml" ]; then
 
-            # Extract the version from pyproject.toml
-            version=$(awk -F'"' '/^version = / {print $2}' "$directory/pyproject.toml")
+#             # Extract the version from pyproject.toml
+#             version=$(awk -F'"' '/^version = / {print $2}' "$directory/pyproject.toml")
 
-            done_branch="done/$directory/$version"
+#             done_branch="done/$directory/$version"
 
-            # Check if the git branch exists
-            if git ls-remote --exit-code --heads origin $done_branch; then
-                echo "Git branch $done_branch exists, DONE!"
-            else
-                echo "Git branch $done_branch does not exist."
-            fi
+#             # Check if the git branch exists
+#             if git ls-remote --exit-code --heads origin $done_branch; then
+#                 echo "Git branch $done_branch exists, NOT_OK!"
+#             else
+#                 echo "Git branch $done_branch does not exist, OK."
+#             fi
+#         fi
+#     fi
+# done
+
+# Get the last commit hash
+last_commit=$(git rev-parse HEAD)
+
+# Find the modified directories using git diff-tree
+modified_directories=$(git diff-tree --no-commit-id --name-only -r "$last_commit" | xargs -I{} dirname {} | sort -u)
+
+# Print the modified directories
+for dir in $modified_directories; do
+    if [ -f "$directory/pyproject.toml" ]; then
+
+        # Extract the version from pyproject.toml
+        version=$(awk -F'"' '/^version = / {print $2}' "$directory/pyproject.toml")
+
+        done_branch="done/$directory/$version"
+
+        # Check if the git branch exists
+        if git ls-remote --exit-code --heads origin $done_branch; then
+            echo "Git branch $done_branch exists, NOT_OK!"
+        else
+            echo "Git branch $done_branch does not exist, OK."
         fi
     fi
 done
