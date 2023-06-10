74c9ae624509379a718614a4b14ae6ec0cae9d21
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-09 22:40:51 -0700

    what about this

--- scripts/check_versions_done.sh
+++ scripts/check_versions_done.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-# # Get the list of child directories
-# directories=$(find . -maxdepth 1 -type d)
-
-# # Iterate over each child directory
-# for directory in $directories; do
-#     # Skip the current directory and parent directory
-#     if [[ $directory != "." && $directory != ".." ]]; then
-#         # Remove the starting './' from the directory name
-#         directory=$(basename "$directory")
-#         # Check if pyproject.toml exists
-#         if [ -f "$directory/pyproject.toml" ]; then
-
-#             # Extract the version from pyproject.toml
-#             version=$(awk -F'"' '/^version = / {print $2}' "$directory/pyproject.toml")
-
-#             done_branch="done/$directory/$version"
-
-#             # Check if the git branch exists
-#             if git ls-remote --exit-code --heads origin $done_branch; then
-#                 echo "Git branch $done_branch exists, NOT_OK!"
-#             else
-#                 echo "Git branch $done_branch does not exist, OK."
-#             fi
-#         fi
-#     fi
-# done
-
 # Get the last commit hash
 last_commit=$(git rev-parse HEAD)
 
@@ -35,12 +8,12 @@ modified_directories=$(git diff-tree --no-commit-id --name-only -r "$last_commit
 
 # Print the modified directories
 for dir in $modified_directories; do
-    if [ -f "$directory/pyproject.toml" ]; then
+    if [ -f "$dir/pyproject.toml" ]; then
 
         # Extract the version from pyproject.toml
-        version=$(awk -F'"' '/^version = / {print $2}' "$directory/pyproject.toml")
+        version=$(awk -F'"' '/^version = / {print $2}' "$dir/pyproject.toml")
 
-        done_branch="done/$directory/$version"
+        done_branch="done/$dir/$version"
 
         # Check if the git branch exists
         if git ls-remote --exit-code --heads origin $done_branch; then
