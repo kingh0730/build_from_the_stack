31d6bbc35961e0900b65bdf76708928c31c7bdec
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-15 21:54:01 -0700

    copy

--- dataset_apps_decode/src/dataset_apps_decode/dataset.py
+++ dataset_apps_decode/src/dataset_apps_decode/dataset.py
@@ -1,4 +1,8 @@
+# Stdlib imports
+import json
+
 # Third-party imports
+import numpy as np
 from datasets import load_from_disk
 
 # Project imports
@@ -30,5 +34,34 @@ class APPSDecode:
 
     def build(self):
         ds = APPSDataset().dataset()
+
+        df = ds.to_pandas()
+
+        df["solutions"] = df["solutions"].apply(
+            lambda x: json.loads(x) if x else [],
+        )
+        df["num_answers"] = df["solutions"].apply(
+            len,
+        )
+        df["input_output"] = df["input_output"].apply(
+            lambda x: json.loads(x) if x else {"inputs": [], "outputs": []},
+        )
+        df["num_tests"] = df["input_output"].apply(
+            lambda x: len(x["inputs"]),
+        )
+
+        df["mean_answer_lines"] = df["solutions"].apply(
+            lambda x: np.mean([len(soln.split("\n")) for soln in x]),
+        )
+        df["std_answer_lines"] = df["solutions"].apply(
+            lambda x: np.std([len(soln.split("\n")) for soln in x]),
+        )
+
+        platforms = df["url"].str.split(".")
+        platforms0 = platforms.str[0].str.split("/").str[-1]
+        platforms0[platforms0.isin(["open", "www"])] = platforms[
+            platforms0.isin(["open", "www"])
+        ].str[1]
+        df["platform"] = platforms0
 
         return ds
