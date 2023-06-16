c6c75c482811992509d079a4dbd16ec721b8e96c
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-15 21:36:33 -0700

    copy

--- dataset_apps_decode/src/dataset_apps_decode/_config.py
+++ dataset_apps_decode/src/dataset_apps_decode/_config.py
@@ -1,4 +1,11 @@
-CACHE_DIR = "F:/Users/kingh/.cache/huggingface/datasets"
+# Stdlib imports
+from os import path, cpu_count
+from importlib.metadata import version
 
-# This dataset has no train/valid/test split
-SPLIT = "train"
+
+BASE_CACHE_DIR = "E:/build_from_the_stack/cache/"
+_dir = __package__ + version(__package__)
+CACHE_DIR = BASE_CACHE_DIR + _dir
+
+
+NUM_PROC = cpu_count()
