068ecf5f3313a0874d197cc982e557f804517826
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-23 13:49:49 -0700

    init

--- data/inputs_gen/dsl/dsl_0.py
+++ data/inputs_gen/dsl/dsl_0.py
@@ -1,1 +1,15 @@
-from dsl import integer
+from dsl import integer, enum, concat_str, record
+
+
+def input_generator():
+    t = integer(1, 10**4)
+    record(t)
+
+    for _ in range(t):
+        n = integer(1, 2 * 10**5)
+        record(n)
+
+        for _ in range(n):
+            word_length = integer(1, 4 * 10**6 // n)
+            word = concat_str([enum("0", "1") for _ in range(word_length)])
+            record(word)
