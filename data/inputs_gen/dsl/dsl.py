25a49e79a5478472519259095364cc036dc131ee
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-23 14:13:37 -0700

    how about

--- data/inputs_gen/dsl/dsl.py
+++ data/inputs_gen/dsl/dsl.py
@@ -12,7 +12,7 @@ def record(arg: int | float | str):
     print(arg)
 
 
-def integer(min_inclusive: int, max_inclusive: int):
+def gen_int(min_inclusive: int, max_inclusive: int):
     """
     Returns a random integer between min and max
     """
@@ -22,7 +22,7 @@ def integer(min_inclusive: int, max_inclusive: int):
     )
 
 
-def float(min_inclusive: float, max_inclusive: float):
+def gen_float(min_inclusive: float, max_inclusive: float):
     """
     Returns a random float between min and max
     """
@@ -32,18 +32,18 @@ def float(min_inclusive: float, max_inclusive: float):
     )
 
 
-def positive_integer(max_inclusive: int):
+def gen_pos_int(max_inclusive: int):
     """
     Returns a random integer between 1 and max
     """
-    return integer(1, max_inclusive)
+    return gen_int(1, max_inclusive)
 
 
-def negative_integer(min_inclusive: int):
+def gen_neg_int(min_inclusive: int):
     """
     Returns a random integer between min and -1
     """
-    return integer(min_inclusive, -1)
+    return gen_int(min_inclusive, -1)
 
 
 def enum(*args):
