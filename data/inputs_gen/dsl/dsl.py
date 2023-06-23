dd748e1e28c4d7f99907f5a813edc51e885402c7
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-23 13:14:17 -0700

    so far

---
+++ data/inputs_gen/dsl/dsl.py
@@ -0,0 +1,37 @@
+"""
+The implementation of these functions is controllable.
+e.g. if you want to change distribution of random numbers
+"""
+import random
+
+
+def integer(min, max):
+    """
+    Returns a random integer between min and max
+    """
+    return random.randint(min, max)
+    raise NotImplementedError
+
+
+def float(min, max):
+    """
+    Returns a random float between min and max
+    """
+    return random.uniform(min, max)
+    raise NotImplementedError
+
+
+def enum(*args):
+    """
+    Returns a random element from the list of args
+    """
+    return random.choice(args)
+    raise NotImplementedError
+
+
+def concat_str(*args):
+    """
+    Returns a string concatenation of all args
+    """
+    return "".join(args)
+    raise NotImplementedError
