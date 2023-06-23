af1844f1f2b8cd0e543520d9eebf049d4b7c047d
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-23 14:18:09 -0700

    how about

--- data/inputs_gen/dsl/dsl_0.py
+++ data/inputs_gen/dsl/dsl_0.py
@@ -1,15 +1,17 @@
-from dsl import integer, enum, concat_str, record
+from dsl import gen_pos_int, enum, concat_str, record
 
 
 def generate_input():
-    t = integer(1, 10**4)
+    t = gen_pos_int(10**4)
     record(t)
 
     for _ in range(t):
-        n = integer(1, 2 * 10**5)
+        n = gen_pos_int(2 * 10**5)
         record(n)
 
         for _ in range(n):
-            word_length = integer(1, 4 * 10**6 // n)
-            word = concat_str(*[enum("0", "1") for _ in range(word_length)])
+            word_length = gen_pos_int(4 * 10**6 // n)
+            word = concat_str(
+                [enum("0", "1") for _ in range(word_length)],
+            )
             record(word)
