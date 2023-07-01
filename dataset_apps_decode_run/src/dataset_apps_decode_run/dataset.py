504ddf90cd76b2dcc52053ea62d49035e292a7ab
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-01 15:39:21 -0700

    fine

--- dataset_apps_decode_run/src/dataset_apps_decode_run/dataset.py
+++ dataset_apps_decode_run/src/dataset_apps_decode_run/dataset.py
@@ -1,8 +1,9 @@
 # Third-party imports
-from datasets import load_dataset
+from datasets import load_from_disk
 
 # Project imports
 from ._config import CACHE_DIR
+from dataset_apps_decode import APPSDecode
 
 
 class APPSDecodeRun:
@@ -10,9 +11,23 @@ class APPSDecodeRun:
         self._ds = None
 
     def loads(self):
-        self._ds = None
+        try:
+            ds = load_from_disk(CACHE_DIR)
+            self.logger.info("Loaded dataset from cache")
+
+        except FileNotFoundError:
+            ds = self.build()
+            ds.save_to_disk(CACHE_DIR)
+            self.logger.info("Saved dataset to cache")
+
+        self._ds = ds
 
     def dataset(self):
         if self._ds is None:
             self.loads()
         return self._ds
+
+    def build(self):
+        apps_decode = APPSDecode().dataset()
+
+        return apps_decode
