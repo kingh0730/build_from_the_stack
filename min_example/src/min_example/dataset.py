81171d336d74f9991998f9e108151dc4cb73b5a4
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-01 15:40:27 -0700

    this

--- min_example/src/min_example/dataset.py
+++ min_example/src/min_example/dataset.py
@@ -1,5 +1,5 @@
 # Third-party imports
-from datasets import load_dataset
+from datasets import load_from_disk
 
 # Project imports
 from ._config import CACHE_DIR
@@ -10,9 +10,21 @@ class MinExample:
         self._ds = None
 
     def loads(self):
-        self._ds = None
+        try:
+            ds = load_from_disk(CACHE_DIR)
+            self.logger.info("Loaded dataset from cache")
+
+        except FileNotFoundError:
+            ds = self.build()
+            ds.save_to_disk(CACHE_DIR)
+            self.logger.info("Saved dataset to cache")
+
+        self._ds = ds
 
     def dataset(self):
         if self._ds is None:
             self.loads()
         return self._ds
+
+    def build(self):
+        raise NotImplementedError
