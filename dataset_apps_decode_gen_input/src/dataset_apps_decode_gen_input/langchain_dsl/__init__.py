79060deefb882e116b1dcc0ac4c2ced6a4df5ef4
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-02 11:03:58 -0700

    say this

--- dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/__init__.py
+++ dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/__init__.py
@@ -1,16 +1,20 @@
 from os import path
 from dotenv import dotenv_values
 
-env = dotenv_values()
-OPENAI_API_KEY = env["OPENAI_API_KEY"]
+_env = dotenv_values()
+
+OPENAI_API_KEY = _env["OPENAI_API_KEY"]
 
 
-DSL_PROMPT_1_INTRO_MD = (
-    path.dirname(
-        path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
-    )
-    + "/data/inputs_gen/dsl/dsl_prompt_1_intro.md"
+_top_dir = path.dirname(
+    path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 )
+
+DSL_PROMPT_1_INTRO_MD = _top_dir + "/data/inputs_gen/dsl/dsl_prompt_1_intro.md"
+DSL_PY = _top_dir + "/data/inputs_gen/dsl/dsl.py"
 
 with open(DSL_PROMPT_1_INTRO_MD, "r") as f:
     DSL_PROMPT_1_INTRO = f.read()
+
+with open(DSL_PY, "r") as f:
+    DSL = f.read()
