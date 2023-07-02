9e31590c6802a142b0ab5f22d244c374bff26046
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-02 10:39:01 -0700

    test

--- dataset_apps_decode_gen_input/tests/test_langchain_dsl.py
+++ dataset_apps_decode_gen_input/tests/test_langchain_dsl.py
@@ -1,8 +1,14 @@
 from dataset_apps_decode_gen_input.langchain_dsl.i_love_programming import (
     i_love_programming,
+    i_love_programming_template,
 )
 
 
 def test_i_love_programming():
     ans = i_love_programming()
+    print(ans)
+
+
+def test_i_love_programming_template():
+    ans = i_love_programming_template()
     print(ans)
