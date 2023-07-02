3b8010478dfb70569b3a1f3040e334c52defded6
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-01 17:55:07 -0700

    copy

---
+++ scripts/install_dirty.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Manually set the last clean commit hash
+last_clean_commit="79fb8bc861da1506717f3ca58c7210bd52aa8caa"
+
+# Get the last commit hash
+last_commit=$(git rev-parse HEAD)
+
+# Find the modified directories using git diff-tree
+modified_directories=$(
+    git diff-tree --no-commit-id --name-only -r \
+        "$last_clean_commit" "$last_commit" |
+        xargs -I {} dirname {} |
+        sort -u
+)
+
+# Print the modified directories
+for dir in $modified_directories; do
+    echo "$dir has been modified."
+
+    if [ -f "$dir/pyproject.toml" ]; then
+        # Extract the version from pyproject.toml
+        version=$(awk -F'"' '/^version = / {print $2}' "$dir/pyproject.toml")
+
+        done_branch="done/$dir/$version"
+
+        # Check if the git branch exists
+        if git ls-remote --exit-code --heads origin $done_branch; then
+            echo "Git branch $done_branch exists, NOT_OK!"
+        else
+            echo "Git branch $done_branch does not exist, OK."
+        fi
+    fi
+done
