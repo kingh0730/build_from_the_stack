e3845795d4b68d9007c4088e0d61de67c0d1ddca
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-01 17:52:14 -0700

    how about

---
+++ dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+
+from .dataset import APPSDecodeGenInput
+
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+    logging.basicConfig(
+        level=logging.INFO,
+    )
+
+    logger = logging.getLogger(__name__)
+
+    apps = APPSDecodeGenInput(
+        logger=logger,
+    )
+
+    ds = apps.dataset()
+
+    return ds
