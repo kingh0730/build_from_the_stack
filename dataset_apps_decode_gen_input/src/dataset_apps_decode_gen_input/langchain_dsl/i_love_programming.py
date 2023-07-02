d654c262eef3d035dc76cdb09552d4b6d7709364
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-02 10:38:28 -0700

    do again

--- dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/i_love_programming.py
+++ dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/i_love_programming.py
@@ -1,11 +1,19 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage,
+)
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 import openai
 from . import OPENAI_API_KEY
 
 
 def i_love_programming():
-    print(openai.api_key)
     chat = ChatOpenAI(
         temperature=0,
         openai_api_key=OPENAI_API_KEY,
@@ -18,3 +26,20 @@ def i_love_programming():
         ]
     )
     return ans
+
+
+def i_love_programming_template():
+    template = "You are a helpful assistant that translates {input_language} to {output_language}."
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    msg = chat_prompt.format_messages(
+        input_language="English", output_language="French", text="I love programming."
+    )
+
+    return msg
