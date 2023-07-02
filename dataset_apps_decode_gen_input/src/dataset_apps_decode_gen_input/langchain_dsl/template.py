0a47dfc1a842cfbd0ac37b12f0fff6152fdbfa12
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-02 10:46:29 -0700

    copy

---
+++ dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/template.py
@@ -0,0 +1,31 @@
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage,
+)
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+import openai
+from . import OPENAI_API_KEY
+
+
+def i_love_programming_template():
+    template = "You are a helpful assistant that translates {input_language} to {output_language}."
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    msg = chat_prompt.format_messages(
+        input_language="English", output_language="French", text="I love programming."
+    )
+
+    return msg
