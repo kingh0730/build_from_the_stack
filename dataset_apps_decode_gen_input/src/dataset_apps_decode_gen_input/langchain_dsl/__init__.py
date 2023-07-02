c2feec7a75fb6e47b21a651f56152e4a60704730
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-02 11:21:23 -0700

    say this

--- dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/__init__.py
+++ dataset_apps_decode_gen_input/src/dataset_apps_decode_gen_input/langchain_dsl/__init__.py
@@ -6,15 +6,58 @@ _env = dotenv_values()
 OPENAI_API_KEY = _env["OPENAI_API_KEY"]
 
 
-_top_dir = path.dirname(
-    path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+_dsl_dir = (
+    path.dirname(
+        path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+    )
+    + "/data/inputs_gen/dsl/"
 )
 
-DSL_PROMPT_1_INTRO_MD = _top_dir + "/data/inputs_gen/dsl/dsl_prompt_1_intro.md"
-DSL_PY = _top_dir + "/data/inputs_gen/dsl/dsl.py"
+DSL_PROMPT_1_INTRO_MD = _dsl_dir + "dsl_prompt_1_intro.md"
+DSL_PROMPT_2_EXAMPLES_INTRO_MD = _dsl_dir + "dsl_prompt_2_examples_intro.md"
+
+DSL_PY = _dsl_dir + "dsl.py"
+
+QUESTION_0_MD = _dsl_dir + "question_0.md"
+DSL_0_PY = _dsl_dir + "dsl_0.py"
+
+QUESTION_1_MD = _dsl_dir + "question_1.md"
+DSL_1_PY = _dsl_dir + "dsl_1.py"
+
 
 with open(DSL_PROMPT_1_INTRO_MD, "r") as f:
     DSL_PROMPT_1_INTRO = f.read()
 
+with open(DSL_PROMPT_2_EXAMPLES_INTRO_MD, "r") as f:
+    DSL_PROMPT_2_EXAMPLES_INTRO = f.read()
+
 with open(DSL_PY, "r") as f:
     DSL = f.read()
+
+with open(QUESTION_0_MD, "r") as f:
+    QUESTION_0 = f.read()
+
+with open(DSL_0_PY, "r") as f:
+    DSL_0 = f.read()
+
+with open(QUESTION_1_MD, "r") as f:
+    QUESTION_1 = f.read()
+
+with open(DSL_1_PY, "r") as f:
+    DSL_1 = f.read()
+
+
+TEMPLATE = f"""
+{DSL_PROMPT_1_INTRO}
+
+```python
+{DSL}
+```
+
+{DSL_PROMPT_2_EXAMPLES_INTRO}
+
+Example 1:
+
+
+
+"""
