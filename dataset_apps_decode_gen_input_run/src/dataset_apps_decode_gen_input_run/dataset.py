4fce3acdacd3d888df18b375b19ccd14061f9576
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-09 12:51:58 -0700

    ok?

--- dataset_apps_decode_gen_input_run/src/dataset_apps_decode_gen_input_run/dataset.py
+++ dataset_apps_decode_gen_input_run/src/dataset_apps_decode_gen_input_run/dataset.py
@@ -6,6 +6,9 @@ from ._config import CACHE_DIR
 from dataset_apps_decode_gen_input import APPSDecodeGenInput
 
 
+DSL_GEN_BASE_DIR = "./data/inputs_gen/dsl/gen/"
+
+
 class APPSDecodeGenInputRun:
     def __init__(self, *, logger):
         self._ds = None
@@ -33,4 +36,27 @@ class APPSDecodeGenInputRun:
             logger=self.logger,
         ).dataset()
 
+        for row in ds:
+            generate_input_gpt_4 = self.process_dsl_py(row["solution: gpt-4"])
+            generate_input_gpt_3 = self.process_dsl_py(row["solution: gpt-3.5-turbo"])
+
+            file_name = f"dsl_{row['problem_id']}.py"
+
+            with open(f"{DSL_GEN_BASE_DIR}/gpt-4/{file_name}", "w") as f:
+                f.write(generate_input_gpt_4)
+
+            with open(f"{DSL_GEN_BASE_DIR}/gpt-3.5-turbo/{file_name}", "w") as f:
+                f.write(generate_input_gpt_3)
+
         return ds
+
+    def process_dsl_py(self, dsl_py: str):
+        stripped = dsl_py[10:-3]
+
+        append = """\
+from dataset_apps_decode_gen_input_run.dsl_impl_copy_to_run import *
+
+
+"""
+
+        return append + stripped
