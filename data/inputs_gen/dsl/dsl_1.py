f603857b3809bd8e6bcb4c3d3f37d60688330a3e
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-08 20:18:24 -0700

    say this

--- data/inputs_gen/dsl/dsl_1.py
+++ data/inputs_gen/dsl/dsl_1.py
@@ -1,17 +1,18 @@
-from dsl import (
+from dsl_impl import (
     gen_pos_int,
-    record,
-    GENERATE_INPUT,
     to_str_then_concat_with_space,
+    GENERATE_INPUT,
 )
 
 
 GENERATE_INPUT
 
 
-def generate_input():
+def generate_input() -> list:
+    res = []
+
     q = gen_pos_int(10**4)
-    record(q)
+    res.append(q)
 
     for _ in range(q):
         ni = gen_pos_int(10**18)
@@ -19,4 +20,4 @@ def generate_input():
         ki = gen_pos_int(10**18)
 
         line = to_str_then_concat_with_space([ni, mi, ki])
-        record(line)
+        res.append(line)
