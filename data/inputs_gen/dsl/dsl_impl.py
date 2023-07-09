329d172614dc10eb52db9fd628a6513565f1d8c0
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-08 19:18:04 -0700

    ok

--- data/inputs_gen/dsl/dsl_impl.py
+++ data/inputs_gen/dsl/dsl_impl.py
@@ -4,4 +4,25 @@ e.g. if you want to change distribution of random numbers
 """
 
 
+# To be overridden
 from dsl import *
+
+
+def gen_int(min_inclusive: int, max_inclusive: int):
+    """
+    Returns a random integer between min (inclusive) and max (inclusive)
+    """
+    return random.randint(
+        min_inclusive,
+        min(max_inclusive, min_inclusive + 4),
+    )
+
+
+def gen_float(min_inclusive: float, max_inclusive: float):
+    """
+    Returns a random float between min (inclusive) and max (inclusive)
+    """
+    return random.uniform(
+        min_inclusive,
+        min(max_inclusive, min_inclusive + 4),
+    )
