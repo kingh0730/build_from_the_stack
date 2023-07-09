cf0674788da7ba7061b9b9ad2d4240895d59e6f3
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-09 12:58:30 -0700

    fix

--- data/inputs_gen/dsl/dsl_impl.py
+++ data/inputs_gen/dsl/dsl_impl.py
@@ -1,12 +1,3 @@
-"""
-The implementation of these functions is controllable.
-e.g. if you want to change distribution of random numbers
-"""
-
-
-import random
-
-
 # To be overridden
 from dsl_declare import *
 
@@ -14,23 +5,3 @@ from dsl_declare import *
 GENERATE_INPUT = """
 Marker for the function that generates a random valid input
 """
-
-
-def gen_int(min_inclusive: int, max_inclusive: int):
-    """
-    Returns a random integer between min (inclusive) and max (inclusive)
-    """
-    return random.randint(
-        min_inclusive,
-        min(max_inclusive, min_inclusive + 4),
-    )
-
-
-def gen_float(min_inclusive: float, max_inclusive: float):
-    """
-    Returns a random float between min (inclusive) and max (inclusive)
-    """
-    return random.uniform(
-        min_inclusive,
-        min(max_inclusive, min_inclusive + 4),
-    )
--- dataset_apps_decode_gen_input_run/src/dataset_apps_decode_gen_input_run/dsl_impl_copy_to_run.py
+++ dataset_apps_decode_gen_input_run/src/dataset_apps_decode_gen_input_run/dsl_impl_copy_to_run.py
@@ -29,3 +29,17 @@ def gen_float(min_inclusive: float, max_inclusive: float):
         min_inclusive,
         min(max_inclusive, min_inclusive + 4),
     )
+
+
+def gen_pos_int(max_inclusive: int) -> int:
+    """
+    Returns a random integer between 1 and max (inclusive)
+    """
+    return gen_int(1, max_inclusive)
+
+
+def gen_neg_int(min_inclusive: int) -> int:
+    """
+    Returns a random integer between min (inclusive) and -1
+    """
+    return gen_int(min_inclusive, -1)
