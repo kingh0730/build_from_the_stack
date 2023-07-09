b193a62d45e20c4900841e9fefa7464429d2478b
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-08 19:17:19 -0700

    move

--- data/inputs_gen/dsl/dsl.py
+++ data/inputs_gen/dsl/dsl.py
@@ -1,7 +1,3 @@
-"""
-The implementation of these functions is controllable.
-e.g. if you want to change distribution of random numbers
-"""
 import random
 
 
---
+++ data/inputs_gen/dsl/dsl_impl.py
@@ -0,0 +1,7 @@
+"""
+The implementation of these functions is controllable.
+e.g. if you want to change distribution of random numbers
+"""
+
+
+from dsl import *
